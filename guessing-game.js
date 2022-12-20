40681da54d81c451c0baef70ed93c101badb9082
nll004 <loninath17@gmail.com>
2022-12-19 20:14:09 -0500

    guessing-game complete and passing tests

---
+++ guessing-game.js
@@ -0,0 +1,21 @@
+
+function guessingGame() {
+    const randNum = Math.floor(Math.random() * 101);
+    let guessCount = 0;
+    let gameFinished = false;
+
+    return function checkGuess(guess){
+        if(gameFinished) return "The game is over, you already won!"
+        else guessCount = guessCount + 1;
+
+        if (guess > randNum) return `${guess} is too high!`;
+        else if (guess < randNum) return `${guess} is too low!`;
+        else {
+            gameFinished = true;
+            return `You win! You found ${guess} in ${guessCount} guesses.`;
+        }
+    }
+}
+
+
+module.exports = { guessingGame };
